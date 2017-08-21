b7e8ff2e2874065cf11cc29af0bfaca44ea0b6ec
gonzaloiv <aelthalion@gmail.com>
2017-08-21 12:19:30 +0200

    Project setup

---
+++ Assets/Scripts/Lib/StateMachine/State.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State : MonoBehaviour {
+
+  #region Mono Behaviour
+  
+  void OnDestroy() {
+    RemoveListeners();
+  }
+  
+  #endregion
+
+  #region Public Behaviour
+
+  public virtual void Enter() {
+    Debug.Log(gameObject.name + " enters " + this.GetType());
+    AddListeners();
+  }
+
+  public virtual void Exit() {
+    RemoveListeners();
+  }
+
+  public virtual void Play() {}
+
+  #endregion
+
+  #region Protected Behaviour
+
+  protected virtual void AddListeners() {}
+
+  protected virtual void RemoveListeners() {}
+
+  #endregion
+
+}
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+    #region Mono Behaviour
+
+    void Awake() {
+        Debug.Log("App awaking...");
+    }
+
+    #endregion
+
+}
